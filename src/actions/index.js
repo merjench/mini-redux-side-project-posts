23d78dbbd4fa27af0bb82cca6355ca360f8c1065
merjench <merjen.chary@gmail.com>
2019-07-05 13:29:38 -0400

    action creator returns an 'action' with fetched data

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    
+    // return {
+    //   type: "FETCH_POSTS",
+    //   payload: promise
+    // };
+    dispatch({ type: 'FETCH_POSTS', payload: response })
+  };
--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
 class PostList extends Component {
+
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
 
   render() {
     return (
@@ -12,4 +18,7 @@ class PostList extends Component {
 
 }
 
-export default PostList;
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
